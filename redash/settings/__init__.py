e789d5de6f9b2cda5b1560fc2f56ca871dbdf29b
于泽 <yuze@yuze.local>
2023-03-10 14:58:36 +0800

    redash 添加 trino

--- redash/settings/__init__.py
+++ redash/settings/__init__.py
@@ -340,6 +340,7 @@ default_query_runners = [
     "redash.query_runner.elasticsearch",
     "redash.query_runner.amazon_elasticsearch",
     "redash.query_runner.presto",
+    "redash.query_runner.trino",
     "redash.query_runner.databricks",
     "redash.query_runner.hive_ds",
     "redash.query_runner.impala_ds",
